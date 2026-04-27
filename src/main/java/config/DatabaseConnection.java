e39d6aa9b755fa2cc9c0e9804511933beadf4ffc
Linda Bergin <linber24@gmail.com>
2026-04-27 04:25:27 +0200

    created LoggerConfig and  using that in DatabaseConnection

--- src/main/java/config/DatabaseConnection.java
+++ src/main/java/config/DatabaseConnection.java
@@ -5,6 +5,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class DatabaseConnection {
 
@@ -13,8 +14,15 @@ public class DatabaseConnection {
     private static final String URL = dotenv.get("db_url");
     private static final String USER = dotenv.get("db_user");
     private static final String PASSWORD = dotenv.get("db_password");
+    private static final Logger logger = LoggerConfig.getLogger();
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+
+            logger.severe(e.getMessage());
+        }
+        return null;
     }
 }
---
+++ src/main/java/config/LoggerConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+
+public class LoggerConfig {
+
+    static Logger logger = Logger.getLogger
+            (LoggerConfig.class.getName());
+
+    static {
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("logs/app.log", true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        fileHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(fileHandler);
+    }
+
+    static Logger getLogger() {
+        return logger;
+    }
+
+}
